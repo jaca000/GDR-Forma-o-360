abf6e18e0605c0604293bf0a801cc31edb620d29
jaca000 <almanso.jose@gmail.com>
2026-08-19 18:23:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gdr-formacao-360-v2';
+const CACHE='gdr-formacao-360-v4';
 const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest','./logo-formacao-gdr.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
