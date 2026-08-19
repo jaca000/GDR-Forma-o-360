648d0cdb332df919c0a32b81b9b995d87488ebe8
jaca000 <almanso.jose@gmail.com>
2026-08-19 18:39:59 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GDR_CONFIG = {
   APP_NAME: 'GDR Formação 360',
-  API_URL: '', // COLAR AQUI O URL /exec DO GOOGLE APPS SCRIPT
+  API_URL: 'https://script.google.com/macros/s/AKfycbz-FBEY1Lg4rshLfoHiRcfI10xyDkvDRSUMBA1Zc2cdBxCkQ0IB5Rs9oR789KmHbbqGOQ/exec',
   CLUB_NAME: 'GDR Faro do Alentejo'
 };
